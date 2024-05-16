c5b37ddf6593ae30989226e4326b9a1ed0099760
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-05-16 06:25:40 +0100

    add import FirestoreModule instead of AngularFirestoreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { FirestoreModule } from '@angular/fire/firestore';
+
+import { environment } from '../environments/environment.development';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    FirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
