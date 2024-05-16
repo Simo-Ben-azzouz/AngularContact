d308279a9ef95111874eca79c2e76824f4218a8b
Simo-Ben-azzouz <si.mo1@hotmail.fr>
2024-05-16 06:23:41 +0100

    add environment and config firebase

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyACiEBJbdwYam5BMCbD9llesUi5eAF8ue4",
+        authDomain: "contactangular-ed0e5.firebaseapp.com",
+        projectId: "contactangular-ed0e5",
+        storageBucket: "contactangular-ed0e5.appspot.com",
+        messagingSenderId: "424876704005",
+        appId: "1:424876704005:web:8ab6e676acd3b24279b76c",
+        measurementId: "G-2BW1SELF7W"
+    }
+};
